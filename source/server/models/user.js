991a6b067126659bfc869e3d53d468cd83263dfd
barre_v <barre_v@epitech.eu>
2016-04-13 10:26:26 +0800

    [Foodie][Server] Restful API V1 -- Add default users seeding to user model

--- source/server/models/user.js
+++ source/server/models/user.js
@@ -2,13 +2,38 @@
 // Dependencies
 
 var mongoose = require('mongoose'); // ORM
+var log      = require('../log')
 
 var UserSchema = new mongoose.Schema({
-    'username' : {type: String, required: true, unique: true},
-    'password' : {type: String, required: true},
-    'email'    : {type: String, required: true, unique: true},
+    username : {type: String, required: true, unique: true},
+    password : {type: String, required: true},
+    email    : {type: String, required: true, unique: true},
     // To complete according to foodie.
-    'role'     : String // customer / owner / admin ??
+    role     : String // customer / owner / admin ??
 });
 
-module.exports = mongoose.model('User', UserSchema);
+var User = mongoose.model('User', UserSchema);
+
+// Seed some default users.
+// -- Since username is unique, should seed only if they are not present.
+// -- Admin account should stay after production launch.
+User({
+    username: 'admin',
+    password: 'adminpwd',
+    email   : 'admin@foodie.com',
+    role    : 'admin'
+}).save(function (err, obj) {
+    if (!err) log('UserModel', 'Default user `' + obj.username + '` created.');
+});
+
+// -- Should be deleted for production launch, account used only while developing the app.
+User({
+    username: 'test',
+    password: 'testpwd',
+    email   : 'test@foodie.com',
+    role    : 'user'
+}).save(function (err, obj) {
+    if (!err) log('UserModel', 'Default user `' + obj.username + '` created.');
+});
+
+module.exports = User;
