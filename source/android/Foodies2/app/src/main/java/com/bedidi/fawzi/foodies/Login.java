ec0c02be8e0122311870575e3d5d2fb170274b62
BEDIDI Fawzi <fawzi.bedidi@epitech.eu>
2016-05-06 15:55:14 +0800

    [Foodies][Application] The sign up work. Save the ID in the shared preferences.

--- source/android/Foodies2/app/src/main/java/com/bedidi/fawzi/foodies/Login.java
+++ source/android/Foodies2/app/src/main/java/com/bedidi/fawzi/foodies/Login.java
@@ -1,13 +1,63 @@
 package com.bedidi.fawzi.foodies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 
 public class Login extends AppCompatActivity {
+
+    public static final String DATAURL = "http://10.0.2.2:8080/api/users";
+    private EditText usernameText;
+    private EditText passwordText;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usernameText = (EditText) findViewById(R.id.username);
+        passwordText = (EditText) findViewById(R.id.password);
+        requestQueue = Volley.newRequestQueue(this);
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final String index = sharedPreferences.getString("id", "");
+        if (!index.equals("")){
+            Toast.makeText(Login.this, "Id : " + index, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void signInOnClick(View view){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, DATAURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(Login.this, "Response is: "+ response.substring(0,500), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("abd", "Error: " + error);
+            }}
+        );
+        stringRequest.setShouldCache(false);
+        requestQueue.add(stringRequest);
+    }
+
+    public void signUpOnClick(View view){
+        Intent intent = new Intent(Login.this, Signup.class);
+        startActivity(intent);
     }
 }
--- source/android/Foodies2/app/src/main/java/com/bedidi/fawzi/foodies/Signup.java
+++ source/android/Foodies2/app/src/main/java/com/bedidi/fawzi/foodies/Signup.java
@@ -1,13 +1,108 @@
 package com.bedidi.fawzi.foodies;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Signup extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+    private RequestQueue requestQueue;
+    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText password2EditText;
+    private Switch roleSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        requestQueue = Volley.newRequestQueue(this);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Toast.makeText(Signup.this, "Session id is: "+ sharedPreferences.getString("idSession", "fail"), Toast.LENGTH_SHORT).show();
+        usernameEditText = (EditText) findViewById(R.id.username);
+        emailEditText = (EditText) findViewById(R.id.email);
+        passwordEditText = (EditText) findViewById(R.id.password);
+        password2EditText = (EditText) findViewById(R.id.password2);
+        roleSwitch = (Switch) findViewById(R.id.restaurantSwitch);
+    }
+
+    private void saveId(JSONObject jsonResponse){
+        try {
+            sharedPreferences.edit().putString("id", jsonResponse.getString("_id")).apply();
+            Log.d("adb", "[Signup.class][saveId]_id saved to sharedPreferences : " + sharedPreferences.getString("id", "error"));
+        } catch (JSONException e) {
+            Log.d("adb", "[JSONException][Signup.class][saveId]" + e.getMessage());
+        }
+    }
+
+    private JSONObject createJsonObject() throws JSONException {
+        final String password = passwordEditText.getText().toString();
+        final String email = emailEditText.getText().toString();
+        final String username = usernameEditText.getText().toString();
+        final boolean isRestaurant = roleSwitch.isChecked();
+        String role;
+        if (isRestaurant){
+            role = "restaurant";
+        } else{
+            role = "customer";
+        }
+        final JSONObject jsonBody = new JSONObject("{\"password\":\"" + password + "\"," +
+                "\"username\":\"" + username +"\"," +
+                "\"email\":\"" + email +"\"," +
+                "\"role\":\"" + role +"\"}");
+        return jsonBody;
+    }
+
+    public void backOnClick(View view){
+        Intent intent = new Intent(Signup.this, Login.class);
+        startActivity(intent);
+    }
+
+    public void signUpOnClick(View view){
+        if (passwordEditText.getText().toString().equals(password2EditText.getText().toString())){
+            JsonObjectRequest jsonObjectRequest;
+            try {
+                jsonObjectRequest = new JsonObjectRequest(Login.DATAURL, createJsonObject(), new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("adb", "[Signup.class][onErrorResponse]SignUp request responded : " + response);
+                        saveId(response);
+                        Intent intent = new Intent(Signup.this, Login.class);
+                        startActivity(intent);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("adb", "[Signup.class][onErrorResponse]SignUp request failed. Returned : " + error);
+                    }
+                });
+            } catch (JSONException e) {
+                Log.d("adb", "[JSONException][Signup.class][signUpOnClick]" + e.getMessage());
+                return ;
+            }
+            requestQueue.add(jsonObjectRequest);
+        } else {
+            Toast.makeText(Signup.this, "The two passwords don't match", Toast.LENGTH_SHORT).show();
+        }
     }
 }
